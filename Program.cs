12dca1bbe61de642eacc643595dbac0c7e47141f
Iryna <kytsenkoiryna33@gmail.com>
2025-11-02 23:33:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Order
+{
+    public void Make() => Console.WriteLine(" Замовлення створено");
+}
+
+class MakeOrderCommand
+{
+    private Order order;
+    public MakeOrderCommand(Order o) { order = o; }
+    public void Execute() => order.Make();
+}
+
+class Program
+{
+    static void Main()
+    {
+        var order = new Order();
+        var cmd = new MakeOrderCommand(order);
+        cmd.Execute();
+    }
+}
+
+
+
+
+
+
+
+
+
